a3744567a0135cbf12b7e952b44ca7a1786acc2a
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2023-01-17 11:14:41 -0600

    check functioning of left and right shift operators in python

---
+++ Bit-manipulationTechniques/LeftRightShift/main.py
@@ -0,0 +1,12 @@
+def main():
+    num = 10
+
+    # a * 2^b
+    print("Left Shift Operator:", num << 2)
+
+    # a / 2^b
+    print("Right Shift Operator:", num >> 2)
+
+
+if __name__ == "__main__":
+    main()
