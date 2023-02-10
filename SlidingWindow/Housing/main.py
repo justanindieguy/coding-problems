8c182a2e6bc0eb91d97d2b4cb9bd2a7410a85944
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2023-02-10 17:21:44 -0600

    create function to obtain housing pairs when an array with negative integers is given in python

--- SlidingWindow/Housing/main.py
+++ SlidingWindow/Housing/main.py
@@ -91,15 +91,52 @@ def housing_smallest_window(l: list[int], k: int):
     return sw
 
 
+def housing_negative_values(l: list[int], k: int):
+    minimum_val = abs(min(l))
+    l[:] = [num + minimum_val for num in l]
+
+    n = len(l)
+    i, j = 0, 1
+
+    cs = l[0]  # Current Sum starts with one element
+
+    """
+    The formula to calculate the new target sum is:
+    k + m * x
+
+    Where:
+      - k is the original sub-array sum
+      - m is the length of the sub-array
+      - x is the absolute value of the minimum value in the original array
+
+    It is starting multiplying by 1 since the first elements has already
+    been taken
+    """
+    ts = k + 1 * minimum_val
+
+    while j < n:
+        cs += l[j]
+        ts += minimum_val
+        j += 1
+
+        while cs > ts and i < j:
+            cs -= l[i]
+            ts -= minimum_val
+            i += 1
+
+        if cs == ts:
+            print(f"({i}, {j - 1})")
+
+
 def main():
     k = int(input())
 
     # l = [2, 1, 4, 1, 3, 2, 1, 1, 2, 1, 3]
+    # l = [1, 3, 2, 1, 4, 1, 3, 2, 1, 1, 2]
     # ps = get_prefix_sum_list(l)
 
-    l = [1, 3, 2, 1, 4, 1, 3, 2, 1, 1, 2]
-    sw = housing_smallest_window(l, k)
-    print(sw)
+    l = [1, -2, 2, -1, 4, 1, 3, -1, -3, 2, 1, 1, 2]
+    housing_negative_values(l, k)
 
 
 if __name__ == "__main__":
