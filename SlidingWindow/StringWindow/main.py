a8cba3c5c04a11cf5787cc96e76fb4462290e506
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2023-02-14 16:19:07 -0600

    solve string window problem in python using sliding window approach

--- SlidingWindow/StringWindow/main.py
+++ SlidingWindow/StringWindow/main.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def get_substrings(s: str):
     substrings = []
 
@@ -37,11 +40,46 @@ def find_window_brute_force(s2: str, substrings: list[str]):
     return smallest_substring
 
 
+def find_window_sliding_window(s: str, pattern: str):
+    freq_s = Counter()
+    freq_pattern = Counter(pattern)
+
+    cnt = 0
+    start = 0
+    start_idx = -1
+    min_so_far = float("inf")
+    window_size = 0
+
+    for i, ch in enumerate(s):
+        freq_s[ch] += 1
+
+        if freq_pattern[ch] != 0 and freq_s[ch] <= freq_pattern[ch]:
+            cnt += 1
+
+        if cnt == len(pattern):
+            while freq_pattern[s[start]] == 0 or freq_s[s[start]] > freq_pattern[s[start]]:
+                freq_s[s[start]] -= 1
+                start += 1
+
+            window_size = i - start + 1
+            if window_size < min_so_far:
+                min_so_far = window_size
+                start_idx = start
+
+    if start_idx == -1:
+        return "No window found!"
+
+    return s[start:start+min_so_far]
+
+
 def main():
     s1 = input("s1: ")
     s2 = input("s2: ")
-    substrings = get_substrings(s1)
-    print(find_window_brute_force(s2, substrings))
+
+    # substrings = get_substrings(s1)
+    # print(get_smallest_substring(s2, substrings))
+
+    print(find_window_sliding_window(s1, s2))
 
 
 if __name__ == "__main__":
