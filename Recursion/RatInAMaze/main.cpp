000a5cf8876f6bd9dc4e535e0f9e83f4eec248c9
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-02 16:25:24 -0600

    add boilerplate and problem description

---
+++ Recursion/RatInAMaze/main.cpp
@@ -0,0 +1,40 @@
+/*
+ * You are given an integer N and a grid of size NxN.
+ * The cells of the grid are numbered row wise from 1 to N^2. Rat wants to
+ * travel from cell number 1 to cell number N^2, and it can move in only right
+ * direction or down direction from a particular cell. There is exactly one path
+ * from source to destination as some cells are blocked. Help the rap to find
+ * the path.
+ *
+ * Input Format:
+ *
+ *   In the function an integer N is given, and a 2D vector consisting of only
+ *   'O's and 'X's is given where 'X' represents blocked cell.
+ *
+ * Output Format:
+ *
+ *   Return a vector of cell numbers of cells in path (in sequence).
+ *
+ * Sample Testcase:
+ *
+ *   Input:
+ *
+ *   4
+ *   O O X O
+ *   O X O O
+ *   O O O X
+ *   X X O O
+ *
+ *   Output:
+ *
+ *   1 5 9 10 11 15 16
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
