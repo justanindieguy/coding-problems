ccd3d855d118cfdadcf428134afe5bb8e461f377
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-03 11:48:21 -0600

    solve search insert position using binary search

--- Arrays/SearchInsertPosition/main.cpp
+++ Arrays/SearchInsertPosition/main.cpp
@@ -25,10 +25,52 @@
  */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int binarySearch(vector<int> &nums, int target)
+{
+    int start = 0, end = nums.size() - 1, mid = 0;
+
+    if (target > nums[end])
+    {
+        return end + 1;
+    }
+    else if (target < nums[start])
+    {
+        return start;
+    }
+
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+
+    return start;
+}
+
 int main()
 {
+    vector<int> nums = {1, 3, 5, 6};
+
+    int target;
+    cin >> target;
+
+    cout << binarySearch(nums, target) << endl;
+
     return 0;
 }
