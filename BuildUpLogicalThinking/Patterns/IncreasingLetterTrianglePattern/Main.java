7cdb8db60326ac2c4583e68e595bc725b6868978
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-03-12 11:29:48 -0600

    solve increasing letter triangle pattern

---
+++ BuildUpLogicalThinking/Patterns/IncreasingLetterTrianglePattern/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    printPattern(n);
+
+    sc.close();
+  }
+
+  private static void printPattern(int n) {
+    for (int i = 0; i < n; i++) {
+      for (char c = 'A'; c <= 'A' + i; c++) {
+        System.out.print(c + " ");
+      }
+
+      System.out.println();
+    }
+  }
+}
